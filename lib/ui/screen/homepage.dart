5addf523341a9281572936f4af02f6716ee87a95
NN <nn@Mac.communityfibre.co.uk>
2026-01-01 19:51:09 +0000

    Removed code

--- lib/ui/screen/homepage.dart
+++ lib/ui/screen/homepage.dart
@@ -74,51 +74,6 @@ class _HomepageState extends State<Homepage> {
                 Text('Featured Books',style: Theme.of(context).textTheme.headlineMedium),
                 ]
               ),
-              /*GridView.builder(
-                clipBehavior: Clip.hardEdge,
-                padding: EdgeInsets.zero,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(), //Stops 
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 0.5,
-                  mainAxisSpacing: 5,
-                  crossAxisSpacing: 5
-                ),
-                itemCount: imageURLs.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    color: Colors.white,
-                    clipBehavior: Clip.hardEdge,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          height: 250,
-                          width: double.infinity,
-                          child: Image.network(
-                            fit: BoxFit.fill,
-                            imageURLs[index],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Title',style: TextStyle(fontWeight: FontWeight.bold,letterSpacing: -1),),
-                              Text('Author',style: TextStyle(letterSpacing: -1,color: Colors.black54,fontWeight: FontWeight.w500),),
-                              Row(children: [Icon(Icons.star,size: 14,), SizedBox(width: 2) ,Text('0.0'), Spacer(),Container(padding: EdgeInsets.symmetric(horizontal: 4,vertical: 0),decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),color: Color(0xFFEBE5DD)), child: Text('Genre',style: TextStyle(fontSize: 12),))]),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  );
-                },
-              ),*/
               if(!bookViewModel.isLoading || _content != null)
                 FavouritesGrid(imageURLs: imageURLs, catalogue: _content!,homepage: true)
               else
