2aa8701850e68d7cdc9639992457a7ab87bf2816
NN <nn@Mac.communityfibre.co.uk>
2026-01-01 19:51:24 +0000

    Deleted files and build changes

--- lib/shared/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:ui';
-
-class Colors {
-  static var backgroundOrange = Color(0xFF);
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,7 +14,7 @@ import 'package:bookhaven/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const BookhavenApp());
+    await tester.pumpWidget( BookhavenApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
